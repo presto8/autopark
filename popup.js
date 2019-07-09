fb78f71f9c603931dcbca5e7625b975820e30765
Preston Hunt <me@prestonhunt.com>
2019-07-08 21:11:40 -0700

    popup links working

--- popup.js
+++ popup.js
@@ -1,35 +1,33 @@
-window.onload = function() {
-    chrome.runtime.getBackgroundPage(function(background) {
-        //var history = document.getElementById('list');
-        // history.innerHTML = background.parkedHistory.map(x => `<li>${x.title}</li>`).toString();
-        getRecentPinboard(background.options.authtoken);
-    });
-};
-
-
 function getRecentPinboard(authtoken) {
     var url = `https://api.pinboard.in/v1/posts/recent?auth_token=${authtoken}&format=json`;
     fetch(url)
         .then(resp => resp.text())
         .then(text => JSON.parse(text))
         .then(function(json) {
-            // for (var i in json.posts) {
-            //     createEntry(i);
-            // }
-
-            posts = json.posts.map(x => `<li>${x.description}</li>`);
-            document.getElementById('list').innerHTML = posts.join(" ");
+            for (var i in json.posts) {
+                 createEntry(json.posts[i]);
+            }
         });
 }
 
 function createEntry(post) {
     var ul = document.getElementById('list');
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(post.href);
-    node.appendChild(textnode);
-    ul.appendChild(node);
+    var li = document.createElement("LI");
+    var text = document.createTextNode(post.description);
+    li.appendChild(text);
+    li.onclick = function() {
+        openPage(post.href);
+    };
+    li.appendChild(text);
+    ul.appendChild(li);
 }
 
 function openPage(url) {
     chrome.tabs.create({url: url});
 }
+
+window.onload = function() {
+    chrome.runtime.getBackgroundPage(function(background) {
+        getRecentPinboard(background.options.authtoken);
+    });
+};
